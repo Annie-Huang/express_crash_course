e651751f3b10d0f55e0c18f538b419375128b0a0
Annie Huang <annie.hn@gmail.com>
2019-05-12 14:00:07 +1000

    Remove /api/members into it's own file.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger');
-const members = require('./Members');
 
 const app = express();
 
@@ -13,26 +12,28 @@ const app = express();
 // // Init middleware
 // app.use(logger);
 
-// Gets All Members
-app.get('/api/members', (req, res) => res.json(members));
-
-// Get Single Member
-app.get('/api/members/:id', (req, res) => {
-    // res.send(req.params.id);
-
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if(found) {
-        // res.json(members.filter(member => member.id === +req.params.id));
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({msg: `No member with the id of ${req.params.id}`})
-    }
-
-
-});
+// // Gets All Members
+// app.get('/api/members', (req, res) => res.json(members));
+//
+// // Get Single Member
+// app.get('/api/members/:id', (req, res) => {
+//     // res.send(req.params.id);
+//
+//     const found = members.some(member => member.id === parseInt(req.params.id));
+//     if(found) {
+//         // res.json(members.filter(member => member.id === +req.params.id));
+//         res.json(members.filter(member => member.id === parseInt(req.params.id)));
+//     } else {
+//         res.status(400).json({msg: `No member with the id of ${req.params.id}`})
+//     }
+//
+//
+// });
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
