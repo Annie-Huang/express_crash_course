c73da637d4c04482d3c2519269cb338f7878052e
Annie Huang <annie.hn@gmail.com>
2019-05-12 15:40:21 +1000

    Add handlebars middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -11,6 +12,10 @@ const app = express();
 
 // // Init middleware
 // app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Body Parser Middleware
 app.use(express.json());
