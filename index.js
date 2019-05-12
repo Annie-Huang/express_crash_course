2a805521d463e9de38bb4c7267cc6b37d0b3ca18
Annie Huang <annie.hn@gmail.com>
2019-05-12 11:14:44 +1000

    Add original url logging for logger

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // });
 
 const logger = (req, res, next) => {
-    console.log('Hello');
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
 };
 
